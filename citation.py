52cb33132aca56024842ce3e1d1792e5fb5954a7
Belteshazzar Luwalekeah <daniel.cooke900@gmail.com>
2024-02-20 01:24:03 -0500

    Added a table display, and fixed the formatting of the commas and periods within the citation

--- citation.py
+++ citation.py
@@ -1,19 +1,54 @@
-"""
-Wieczner, Jen, and Daniel L Cooke. 
-“Commanders in Chief: The Women Building America’s Military Machine.” 
-Fortune, Fortune, 7 June 2021, 
-fortune.com/longform/lockheed-martin-boeing-women-defense-ceos-trump/. 
-Highly recommend this place
-"""
-
 # import streamlit as st
 import pandas as pd
 from datetime import date
 import streamlit as st
 
+PAGE_TITLE = "Luwahs Citation Machine"
+PAGE_ICON = "📚"
+
+st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
+
+# App Title and other informaiton
+# Create an empty DataFrame for citations if it doesn't exist
+if 'citations_df' not in st.session_state:
+    st.session_state.citations_df = pd.DataFrame(columns=['Citations Generated'])
+
+# Check if it's the user's first time
+if 'first_time' not in st.session_state:
+    # Display the welcome message with background color and wrapping
+    welcome_message = """
+    <div style="background-color: #87CEEB; padding: 10px; border-radius: 5px; margin-bottom: 10px; text-align: center;">
+    Coming soon: A faster way to cite websites and other sources, including books.
+    </div>
+    """
+    st.markdown(welcome_message, unsafe_allow_html=True)
+
+    # Split the layout into columns
+    columns = st.columns(5)
+
+    # Add dummy columns if needed (skip if not required)
+    for _ in range(3):
+        columns[0].text("")  # Add empty content to the first two columns
+
+    # Add a button to close the message in the middle column
+    close_button = columns[2].button("Close")
+
+    # Check if the button is clicked
+    if close_button:
+        # Mark that the user has seen the popup
+        st.session_state.first_time = True
+        # Close the expander immediately
+        st.rerun()
+
+st.markdown("<h1 style='text-align: center;'>MLA9 Citation Machine for Websites</h1>", unsafe_allow_html=True)
+
+# adding some spacing
+st.write("\n")
+st.write("<p style='text-align:center;'>Provide as much information as you can to ensure a thorough citation.</p>", unsafe_allow_html=True)
+
+# App begins here:
 article_title = st.text_input('Article Title', '')
 
-st.divider()
 st.write('Contributors')
 
 wastebin_index = 0
@@ -93,7 +128,10 @@ translators = []
 for contributor_role, first_name, middle_name, last_name in contributors:
     if contributor_role == "Author":
         author_count += 1
-        if first_author:
+        if first_author and not middle_name:
+            author_format = f"{last_name}, {first_name}"
+            first_author = False
+        elif first_author:
             author_format = f"{last_name}, {first_name} {middle_name}"
             first_author = False
         else:
@@ -101,7 +139,9 @@ for contributor_role, first_name, middle_name, last_name in contributors:
         authors.append(author_format)
     elif contributor_role == "Editor" and not edited_by_shown:
         editor_count += 1
-        if first_editor:
+        if not first_editor:
+            first_editor = False
+        elif first_editor:
             editor_format = f"Edited by {first_name} {middle_name} {last_name}"
             first_editor = False
         else:
@@ -120,17 +160,20 @@ for contributor_role, first_name, middle_name, last_name in contributors:
 if author_count > 0:
     authors[-1] += "."  # Add period only to the last "Author" contributor
 
-# Add 'and' between multiple editors and translators
+# Concatenate the lists in the desired order
 editors_format = " ".join(editors)
 translators_format = " ".join(translators)
+authors_format = " ".join(authors)
 
 # Add a comma or period after the last editor based on the presence of translators
-if editors_format and not translators_format:
+if not editors_format and not translators_format:
+    editors_format = ''
+elif not translators_format:
     editors_format += ","
-elif translators_format and not editors_format:
+elif not editors_format:
     translators_format += ","
 else:
-    editors_format += "."
+    editors_format += ","
     translators_format += ","
 
 # Order the contributors as needed
@@ -141,36 +184,79 @@ st.button('Add Contributor', on_click=increase_rows)
 # Display the ordered contributors
 st.write(f"Contributors: {' '.join(ordered_contributors)}")
 
-
-#-------------
-
 # Adding space between the sections
 st.write('\n')
-st.divider()
 
-st.write('Online Publication Info')
+# Order the contributors as needed
+ordered_contributors = authors + [f'"{article_title}"'] + [editors_format] + [translators_format]
+
+# Online Publication Info
 website_title = st.text_input('Website Title', '')
 publisher_sponsor = st.text_input('Publisher/Sponsor', '')
 website_URL = st.text_input('URL', '')
 
-# Add a button to include or remove the url from the list
+# Add radio buttons to include or remove the url from the list
+include_url = st.radio('Include URL in Contributors', ('Yes', 'No'))
 
 # Dates
 published_date = st.date_input("Published Date")
 
-# getting todays date as default
+# getting today's date as default
 date_accessed = st.date_input("Date Accessed", date.today())
 
-st.write('You accessed this site on: ', date_accessed)
+# More Options Expander
+with st.expander("More Options"):
+    annotation = st.text_area('Add Annotation', '')
 
-# add another divider
-st.divider()
+if website_title:
+    ordered_contributors += [f'{website_title}, ']
+    if publisher_sponsor and publisher_sponsor != website_title:
+        ordered_contributors += [f'{publisher_sponsor}, ']
 
-st.expander("")
+if published_date:
+    ordered_contributors += [f' {published_date}']
+
+# Check if URL should be included
+if include_url == 'Yes':
+    ordered_contributors += [f'{website_URL}.']
+elif include_url == 'No':
+    ordered_contributors += [f'']
+
+# look at dates
+if date_accessed:
+    ordered_contributors += [f'{date_accessed}.']
     
+# Add annotation if it is not empty
+if annotation:
+    ordered_contributors += [f' {annotation}.']
 
-# citation_string = 
-# cite = st.text(f'{middle_name} {last_name},{first_name} {name_suffix}."{article_title}"{website_title},{publisher_sponsor},{published_date},{website_URL}.{date_accessed}')
+st.write(f"Contributors: {' '.join(ordered_contributors)}")
+
+# Create an empty DataFrame
+df = pd.DataFrame(columns=['Citations Generated'])
+
+
+# Button to add final ordered contributor to the DataFrame
+if st.button('Cite'):
+    # Initialize the DataFrame if it doesn't exist
+    if 'citations_df' not in st.session_state:
+        st.session_state.citations_df = pd.DataFrame(columns=['Citations Generated'])
+
+    # Append the final ordered contributor to the DataFrame
+    new_row = pd.DataFrame({'Citations Generated': [''.join(ordered_contributors)]})
+    st.session_state.citations_df = pd.concat([st.session_state.citations_df, new_row], ignore_index=True)
+    st.toast('Hooray!', icon='🎉')
+
+
+    # Display the DataFrame with the "Copy" buttons
+    st.table(st.session_state.citations_df)
 
 
 
+# Button to clear sources cited
+if st.button('Clear Sources Cited'):
+    # Clear the DataFrame
+    df = pd.DataFrame(columns=['Citations Generated'])
+
+    # Display the message
+    st.warning('Nothing cited yet', icon="⚠️")
